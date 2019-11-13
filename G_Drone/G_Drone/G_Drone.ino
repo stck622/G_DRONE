9f298ab403a0984b1195cf13b6529f85d1028535
stck622 <stck622@naver.com>
2019-11-13 17:31:58 +0900

    Project make

---
+++ G_Drone/G_Drone/G_Drone.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		G_Drone.ino
+ Created:	2019-11-13 오후 5:29:59
+ Author:	stck6
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
